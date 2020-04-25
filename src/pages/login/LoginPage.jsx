9300a74e7843a1201fb744ba6110113d291d57fa
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-04-25 14:07:51 +0700

    Refactor Login Page SCSS

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class LoginPage extends Component {
   render() {
     return (
       <div className="login-page">
         <div className="form-box"></div>
-        Login Page
-        <Link to="/">To Home</Link>
       </div>
     );
   }
