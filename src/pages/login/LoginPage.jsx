cdccec9c55c9e65eead6d0a3f3c7233972e6145e
Jeremy Yonathan Chrisyanto <Jychrisyanto5@gmail.com>
2020-04-28 00:20:02 +0700

    add quotes in register and login

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -37,7 +37,15 @@ class LoginPage extends Component {
             </div>
           </div>
           <div className="col-lg-6 image">
-            <div className="layer"></div>
+            <div className="layer">
+              <div className="quotes">
+                <div className="doubleQuotes"></div>
+                <h4>LIFE'S TOO SHORT</h4>
+                <h1>TO WEAR BORING CLOTHES.</h1>
+                <h5>- CARLY CUSHNIE AND MICHELLE OCHS,</h5>
+                <h5> CUSHINE ET OCHS -</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
