9fb18dcf6873e04c0a69f25e4b2a2b33f69c73dc
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-04-27 19:14:48 +0700

    Refactor LoginPage and RegisterPage

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
 
 class RegisterPage extends Component {
   render() {
     return (
-      <div className="register-page">
-        <div className="form-box">
-          <h1>Join us now!</h1>
+      <div className="register-page container-fluid">
+        <div className="row">
+          <div className="col-lg-6 form-box">
+            <div className="container">
+              <Link to="/">
+                <img src={Logo} alt="logo-pindia"></img>
+              </Link>
+              <form className="container">
+                <h1>Join us now!</h1>
+              </form>
+            </div>
+          </div>
+          <div className="col-lg-6 image">
+            <div className="layer"></div>
+          </div>
         </div>
-        <div className="layer"></div>
-        <div className="image"></div>
       </div>
     );
   }
