fabe56a6cc3655e67fc6365f61d8c92f1a4c058e
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-05-02 16:13:39 +0700

    Bugfix the shop page

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 
-const ProductCard = ({ name }) => {
+const ProductCard = ({ product }) => {
   return (
     <div className="product-card">
       <div className="card">
         <img src="" className="card-img-top" alt="..."></img>
         <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">{product.description}</p>
+          <div className="btn btn-primary">Go somewhere</div>
         </div>
       </div>
     </div>
